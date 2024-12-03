cddfda746dc30ef227ec8b6def60779fdd0a5016
Maurits "Mousetail" van Riezen <mousetail@mousetail.nl>
2024-12-03 14:24:28 +0100

    update install and download scripts

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for plugin-tcc.
-GH_REPO="https://github.com/mousetial/asdf-plugin-tcc"
-TOOL_NAME="plugin-tcc"
+# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for Tcc.
+TOOL_NAME="TCC"
 TOOL_TEST="tcc --version"
 
 fail() {
@@ -14,26 +13,16 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if plugin-tcc is not hosted on GitHub releases.
-if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
-fi
-
 sort_versions() {
 	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
-
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if plugin-tcc has other means of determining installable versions.
-	list_github_tags
+	curl -s https://mirror.accum.se/mirror/gnu.org/savannah/tinycc/ | \
+		grep -oE "\"tcc-([[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+)\.tar.bz2\"" | \
+		grep -oE "[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+" | \
+		uniq
 }
 
 download_release() {
@@ -42,7 +31,7 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for plugin-tcc
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	url="https://mirror.accum.se/mirror/gnu.org/savannah/tinycc/gcc-{version}.tar.bz2"
 
 	echo "* Downloading $TOOL_NAME release $version..."
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -51,7 +40,7 @@ download_release() {
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -61,10 +50,14 @@ install_version() {
 		mkdir -p "$install_path"
 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert plugin-tcc executable exists.
+		cd "$install_path"
+		./configure --strip-binaries --prefix-dir "$install_path"
+		make
+
+		# TODO: Assert tcc executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
 	) || (
